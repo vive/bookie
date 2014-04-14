90cd04922dca13437fd2a1fa39579dd928cf363b
Jake Craige <jake@poeticsystems.com>
2014-04-14 11:37:23 -0500

    change date conversion to default to eastern time that will allow it to properly adjust for daylights savings time

--- lib/booker/helpers.rb
+++ lib/booker/helpers.rb
@@ -3,10 +3,10 @@ require 'active_support/core_ext/date/zones'
 module Booker
   module Helpers
     # Formats a ruby date into the format the Booker wants it in their json API
-    def self.format_date time, offset = -5
+    def self.format_date time, zone = 'Eastern Time (US & Canada)'
       return nil unless time
 
-      "/Date(#{(time.in_time_zone(offset)).to_i * 1000})/"
+      "/Date(#{(time.in_time_zone(zone)).to_i * 1000})/"
     end
 
     # Turn date given by booker api into ruby datetime
